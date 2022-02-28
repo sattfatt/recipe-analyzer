4200226923470de0c4bf91c3e39efb3fb2234013
sattfatt <sattfatt@gmail.com>
2022-02-27 19:52:11 -0800

    Style changes

--- recipe-analyzer-ui/src/Components/ProfileBox.js
+++ recipe-analyzer-ui/src/Components/ProfileBox.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import {useCookies} from "react-cookie";
 
+import {MdLogout} from "react-icons/md"
 
 function ProfileBox(props) {
+
+    const [toggle, setToggle] = useState(false);
+    const [logoutElement, setLogoutElement] = useState();
+    const [cookies, setCookie, removeCookie] = useCookies(['token', 'name', 'email']);
+    
+    const logout = () => {
+        removeCookie('token');
+        removeCookie('name');
+        removeCookie('email');
+        window.location.reload(false);
+    }
+
+    const onClick = () => {
+        if (!logoutElement) {
+            setLogoutElement(<button style={{background:"red", color:"white"}} id="logout-button" onClick={logout}>Logout &nbsp;<MdLogout/></button>)
+        } else {
+            setLogoutElement();
+        }
+    }
+
+
     return (
-        <div id="profile-container">
+        <div onClick={onClick} id="profile-container">
             <div>{props.cookies.name}</div>
+            {logoutElement}
         </div>
     );
 }
